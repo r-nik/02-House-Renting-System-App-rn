53707accb6c94c875a657cd016ce5e4647ce76ba
r-nik <ru_nikolova@yahoo.com>
2026-04-14 20:32:49 +0200

    Projektdateien hinzufügen.

---
+++ HouseRentingSystem.Services/Houses/Models/HouseCategoryServiceModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRentingSystem.Services.Houses.Models
+{
+    public class HouseCategoryServiceModel
+    {
+        public int Id { get; init; }
+        public string? Name { get; init; }
+    }
+}
---
+++ HouseRentingSystem.Tests/UnitTests/AgentServiceClass.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework.Legacy;
+using HouseRentingSystem.Services.Agents;
+
+namespace HouseRentingSystem.Tests.UnitTests
+{
+    [TestFixture]
+    public class AgentServiceTests : UnitTestsBase
+    {
+        private IAgentService agentService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+            => this.agentService = new AgentService(this.data);
+
+        [Test]
+        public void GetAgentId_ShouldReturnCorrectUserId()
+        {
+            // Arrange
+
+            // Act: invoke the service method with valid id
+            var resultAgentId = this.agentService.GetAgentId(this.Agent.UserId);
+
+            // Assert a correct id is returned
+            Assert.That(resultAgentId, Is.EqualTo(this.Agent.Id));
+        }
+
+       [Test]
+        public void ExistsById_ShouldReturnTrue_WithValidId()
+        {
+            // Arrange
+
+            // Act: invoke the service method with valid agent id
+            var result = this.agentService.ExistsById(this.Agent.UserId);
+
+            // Assert the method result is true
+            Assert.That(result, Is.True);
+        }
+
+       
+        [Test]
+        public void AgentWithPhoneNumberExists_ShouldReturnTrue_WithValidData()
+        {
+            // Arrange
+
+            // Act: invoke the service method with valid agent phone num
+            var result = this.agentService
+                .AgentWithPhoneNumberExists(this.Agent.PhoneNumber);
+
+            // Assert the method result is true
+            Assert.That(result, Is.True);
+        }
+
+        
+        [Test]
+        public void CreateAgent_ShouldWorkCorrectly()
+        {
+            // Arrange: get all agents' current count
+            var agentsCountBefore = this.data.Agents.Count();
+
+            // Act: invoke the service method with valid data
+            this.agentService.Create(this.Agent.UserId, this.Agent.PhoneNumber);
+
+            // Assert the agents' count has increased by 1
+            var agentsCountAfter = this.data.Agents.Count();
+            Assert.That(agentsCountAfter, Is.EqualTo(agentsCountBefore + 1));
+
+            // Assert a new agent was created in the db with correct data
+            var newAgentId = this.agentService.GetAgentId(this.Agent.UserId);
+            var newAgentInDb = this.data.Agents.Find(newAgentId);
+            Assert.That(newAgentInDb, Is.Not.Null);
+            Assert.That(newAgentInDb.UserId, Is.EqualTo(this.Agent.UserId));
+            Assert.That(newAgentInDb.PhoneNumber, Is.EqualTo(this.Agent.PhoneNumber));
+        }
+    }
+}
